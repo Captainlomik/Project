38b7d5babb2baf719d1b6126901754ee12219fc4
captainlomik <nastiy248@mail.ru>
2023-10-12 22:39:01 +0300

    add page

---
+++ src/app/coupens/copen-list/copen-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-copen-list',
+  templateUrl: './copen-list.component.html',
+  styleUrls: ['./copen-list.component.scss']
+})
+export class CopenListComponent {
+
+}
