44779996b0ae6aaec588cd74cb5ab281fed11e2f
captainlomik <nastiy248@mail.ru>
2023-10-12 22:38:15 +0300

    add new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,9 +10,15 @@ import { SettingsComponent } from './settings/settings.component';
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'products', component: ProductsComponent },
+  {
+    path: 'products',
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: 'coupens',
+    loadChildren: () => import('./coupens/coupens.module').then(m => m.CoupensModule)
+  },
   { path: 'statistics', component: StatisticsComponent },
-  { path: 'coupens', component: CoupensComponent },
   { path: 'media', component: MediaComponent },
   { path: 'settings', component: SettingsComponent },
 ];
